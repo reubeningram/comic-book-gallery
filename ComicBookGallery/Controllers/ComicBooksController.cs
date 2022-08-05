a1b8a9a5d396ed1f7f6af663c06748d7cc2c2d84
ringram <reuben.ringram@gmail.com>
2022-08-05 17:49:10 -0500

    Added Comic Book Detail book

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
- 
+
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail() {
-            return "Hello from the Comic Book";
-        
-        }
+        public ActionResult Detail()
+        {
+
+            return View();
+         }
     }
 }
+
