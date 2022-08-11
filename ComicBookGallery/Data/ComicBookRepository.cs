69e57ec24af6fc6303f3b798c4e0f72ef02788e8
ringram <reuben.ringram@gmail.com>
2022-08-10 22:56:38 -0500

    Added Comic Books list view

--- ComicBookGallery/Data/ComicBookRepository.cs
+++ ComicBookGallery/Data/ComicBookRepository.cs
@@ -60,6 +60,10 @@ namespace ComicBookGallery.Data
     
             };
 
+        public ComicBook[] GetComicBooks() {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
